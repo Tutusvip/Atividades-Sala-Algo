ac84195aaaa7c470e7303114775139369a63c7eb
tutusvip <111453250+Tutusvip@users.noreply.github.com>
2022-10-27 13:32:45 -0300

    Add files via upload

---
+++ impaparfor.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	int i,valor, rs;
+	
+	printf("Digite um valor: ");
+	scanf("%d", &valor);
+	
+
+	
+	for(i=0;i<=100;i=i+2){
+	printf("%d\t", i);
+		
+	}
+	
+	printf("\n--------------------------------");
+	printf("\n\tFIM DO CODIGO");
+	return (0);	
+ }
