ad9e447220d7a2496138a1bb061f3eedee8bddf9
tutusvip <111453250+Tutusvip@users.noreply.github.com>
2022-10-27 13:59:24 -0300

    Add files via upload

---
+++ multiplicacaoemcasacata.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int i,multoplicacao = 1,numero;
+	
+	for(i=1;i<5;i++){
+		scanf("%d\t", &numero);
+		multoplicacao=multoplicacao*numero;
+		
+	}
+		printf("\nmultoplicacao: %d, soma");
+
+	
+ }
---
+++ somaemcasacata.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	int i,soma = 0,numero;
+	
+	for(i=0;i<5;i++){
+		scanf("%d\t", &numero);
+		soma=soma+numero;
+		
+	}
+		printf("\nSoma: %d, soma");
+
+	
+ }
